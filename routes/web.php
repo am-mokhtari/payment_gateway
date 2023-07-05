61c294f974bdfacc8d35c9035fec436e41b8db5e
amirmohammadrtz <amirmohammad.m509@gmail.com>
2023-07-05 20:47:35 +0330

    Redirect the user to the login page first and remove the welcome page and add all needed routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
@@ -26,6 +27,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('/pay')->group(function () {
+        Route::get('{order}', [OrderController::class, 'create'])
+            ->whereNumber('order')->name('pay');
+
+        Route::get('/result', [OrderController::class, 'store']);
+
+        Route::get('/receipt/{transaction}', [OrderController::class, 'show'])->name('receipt');
+        Route::get('/transactions', [OrderController::class, 'index'])->name('transactions');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
